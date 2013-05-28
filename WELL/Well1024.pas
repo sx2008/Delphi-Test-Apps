b050ef3f3871a4c9bd63c9b44179f89b7a492668
sx2008 <sx2008@web.de>
2013-05-28 15:18:33 +0200

    added copyright of the original authors of the WELL algorithm

--- WELL/Well1024.pas
+++ WELL/Well1024.pas
@@ -1,7 +1,7 @@
 unit Well1024;
 {
 Delphi Implementation of
-WELL 1024 Random Number Generator
+WELL 1024a Random Number Generator
 
 see:
 http://de.wikipedia.org/wiki/Well_Equidistributed_Long-period_Linear
@@ -14,49 +14,14 @@ interface
 uses Classes;
 
 (*
-
-#define W 32
-#define R 32
-#define M1 3
-#define M2 24
-#define M3 10
-
-#define MAT0POS(t,v) (v^(v>>t))
-#define MAT0NEG(t,v) (v^(v<<(-(t))))
-#define Identity(v) (v)
-
-#define V0            STATE[state_i                   ]
-#define VM1           STATE[(state_i+M1) & 0x0000001fU]
-#define VM2           STATE[(state_i+M2) & 0x0000001fU]
-#define VM3           STATE[(state_i+M3) & 0x0000001fU]
-#define VRm1          STATE[(state_i+31) & 0x0000001fU]
-#define newV0         STATE[(state_i+31) & 0x0000001fU]
-#define newV1         STATE[state_i                   ]
-
-#define FACT 2.32830643653869628906e-10
-
-static unsigned int state_i = 0;
-static unsigned int STATE[R];
-static unsigned int z0, z1, z2;
-
-void InitWELLRNG1024a (unsigned int *init){
-   int j;
-   state_i = 0;
-   for (j = 0; j < R; j++)
-     STATE[j] = init[j];
-}
-
-double WELLRNG1024a (void){
-  z0    = VRm1;
-  z1    = Identity(V0)       ^ MAT0POS (8, VM1);
-  z2    = MAT0NEG (-19, VM2) ^ MAT0NEG(-14,VM3);
-  newV1 = z1                 ^ z2;
-  newV0 = MAT0NEG (-11,z0)   ^ MAT0NEG(-7,z1)    ^ MAT0NEG(-13,z2) ;
-  state_i = (state_i + 31) & 0x0000001fU;
-  return ((double) STATE[state_i]  * FACT);
-}
-
-
+orginal Code see: http://www.iro.umontreal.ca/~panneton/well/WELL512a.c
+/* ***************************************************************************** */
+/* Copyright:      Francois Panneton and Pierre L'Ecuyer, University of Montreal */
+/*                 Makoto Matsumoto, Hiroshima University                        */
+/* Notice:         This code can be used freely for personal, academic,          */
+/*                 or non-commercial purposes. For commercial purposes,          */
+/*                 please contact P. L'Ecuyer at: lecuyer@iro.UMontreal.ca       */
+/* ***************************************************************************** */
 *)
 
 
@@ -74,7 +39,6 @@ type
     procedure Assign(Source: TPersistent);override;
     function Random:Cardinal;
     procedure Randomize;
-
   end;
 
 
